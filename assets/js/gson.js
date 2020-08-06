abc8a0ab0b156b52021060c18d8ebce8c0621fcd
Jordanr <jordan@2talk.co.nz>
2020-08-06 16:39:28 +1200

    blah

--- assets/js/gson.js
+++ assets/js/gson.js
@@ -2,7 +2,7 @@ import fetch from 'axios';
 import mock from './mock.json';
 
 export async function getData(sheetsURL) {
-  const gson = `https://gson.now.sh?url=https://docs.google.com/spreadsheets/d/e/2PACX-1vRo_gRo2HIWYD5adpZdIFY2qGpWdWw-eQ0-kj0yR25DjlMzLZSZ7a70-nOq6EsTw2NugHxriv_9TJiw/pub?gid=0&single=true&output=csv`;
+  const gson = `https://gson.now.sh?url=https://docs.google.com/spreadsheets/d/e/2PACX-1vRo_gRo2HIWYD5adpZdIFY2qGpWdWw-eQ0-kj0yR25DjlMzLZSZ7a70-nOq6EsTw2NugHxriv_9TJiw/pub?output=csv`;
 
   const grab = await fetch(gson);
 
