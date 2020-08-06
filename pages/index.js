0d1b206b9ed18be75a06b759de3def83db05a677
Jordanr <jordan@2talk.co.nz>
2020-08-06 17:15:17 +1200

    update

--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,8 @@ export default function Index({ gson, lastUpdate, preview }) {
       <section id='section-table'>
         <div className='header-bar'>
           <Item
-            pic={'inv_misc_monsterhead_04.jpg'}
-            id={6657}
+            pic={'inv_misc_head_human_01.jpg'}
+            id={5442}
             total={table.ally.total.sumTotal + table.horde.total.sumTotal}
             current={table.ally.current.sumTotal + table.horde.current.sumTotal}
           />
